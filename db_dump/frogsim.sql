96f83c3d92c6c09d105e7cacbd6ef65650e05781
ArvisTech <udegbeandre@gmail.com>
2017-06-01 03:10:26 +0100

    sql_dump

--- db_dump/frogsim.sql
+++ db_dump/frogsim.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2017-05-31 17:36:32
+Date: 2017-06-01 03:08:54
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,12 +23,11 @@ CREATE TABLE `frog_tbl` (
   `frog_id` int(11) NOT NULL AUTO_INCREMENT,
   `frog_specie_id` int(11) DEFAULT NULL,
   `frog_gender` varchar(255) DEFAULT NULL,
-  `frog_color` varchar(255) DEFAULT NULL,
   `frog_weight` double DEFAULT NULL,
   `frog_age` varchar(255) DEFAULT NULL,
   `frog_pond_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`frog_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for pond_tbl
@@ -40,7 +39,7 @@ CREATE TABLE `pond_tbl` (
   `pond_capacity` varchar(255) NOT NULL,
   `pond_size` varchar(255) NOT NULL,
   PRIMARY KEY (`pond_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for specie_tbl
@@ -54,4 +53,4 @@ CREATE TABLE `specie_tbl` (
   `specie_life_expect` int(11) NOT NULL,
   `specie_crowd_rate` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`specie_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
